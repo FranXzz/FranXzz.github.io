fc170bcad5723a9b2540e303562246e0cc0ab78e
Franco Milazzo <franquimilazzo@Icloud.com>
2025-02-04 02:46:35 +0100

    Web fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Reemplaza con el nombre de tu repositorio
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/', // Asegúrate de que las rutas sean relativas a la raíz del dominio
 });
